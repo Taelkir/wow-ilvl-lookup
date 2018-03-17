39c9e88622aaecd864230b9f9cb25a9a094c238d
Tom <tom.stanley37@gmail.com>
2018-03-17 14:25:54 +0000

    Button fetches first character in the guild and prints name and level

--- ilvl-lookup-js.js
+++ ilvl-lookup-js.js
@@ -1,31 +1,19 @@
 // Create a const for the button
 const refreshButton = document.querySelector(".refresh-button");
-
-// Function shamlessly used from https://stackoverflow.com/a/18278346 because I don't understand jQuery
-function loadJSON(path, success, error)
-{
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
-    };
-    xhr.open("GET", path, true);
-    xhr.send();
-}
+// Create a const for the target UL
+const characterList = document.querySelector(".character-list");
 
 // Listen for the button to be pressed and call and return the data we need using the above function
 refreshButton.addEventListener("click", () => {
   console.log("Thanks for clicking that button! Now to get to work.");
-  loadJSON('https://eu.api.battle.net/wow/guild/argent-dawn/Cutthroat%20Comrades?fields=members&locale=en_GB&apikey=wr4u5hb5magzc44cc8usfum542fq7p3j',
-           function(data) { console.log(data); },
-           function(xhr) { console.error(xhr); }
-  );
+  // This bit stolen from https://stackoverflow.com/a/11116930 because I don't understand jQuery
+  var xhReq = new XMLHttpRequest();
+  xhReq.open("GET", "https://eu.api.battle.net/wow/guild/argent-dawn/Cutthroat%20Comrades?fields=members&locale=en_GB&apikey=wr4u5hb5magzc44cc8usfum542fq7p3j", false);
+  xhReq.send(null);
+  var jsonObject = JSON.parse(xhReq.responseText);
+  // Code copy&paste finished.
+  // Now have the JSON object stored in a variable called jsonObject
+
+  characterList.innerHTML = "<li><em>Name</em>: " + jsonObject.members[0].character.name + ", <em>Character Level</em>:" + jsonObject.members[0].character.level + "</li>";
+
 })
