5cec0077c8448a0df9a2cf0ac0cf51e518ffeaa7
Tom <tom.stanley37@gmail.com>
2018-03-24 17:34:07 +0000

    Change GET requests to use jQuery .getJSON

--- ilvl-lookup-js.js
+++ ilvl-lookup-js.js
@@ -1,59 +1,56 @@
 const refreshButton = document.querySelector(".refresh-button"); // Create a const for the button
 const characterList = document.querySelector(".character-list"); // Create a const for the target <ul>
 var infoDiv = document.querySelector(".info-div"); // For hiding later
-var loadingDiv = document.querySelector(".loading-spinner"); // For showing while loading
-var jsonObjectGuild = {}; // Store the guild-level JSON request here
-var jsonObjectCharacter = {}; // Store the character-level JSON request here
+let jsonObjectGuild = {}; // Store the guild-level JSON request here
+var jsonObjectCharacters = []; // Store the character-level JSON requests here
 var toPrint = ""; // Build up the <li> elements here filled with juicy information
 var characterName = {}; // Array to store character names in for requesting their ilvl later
+var xhReqGuild;
+var xhReqChar;
 
 // Call the guild API (used once)
 function guildApiCall (){
-  var xhReqGuild = new XMLHttpRequest();
-  xhReqGuild.open("GET", "https://eu.api.battle.net/wow/guild/argent-dawn/Cutthroat%20Comrades?fields=members&locale=en_GB&apikey=wr4u5hb5magzc44cc8usfum542fq7p3j", true);
-  xhReqGuild.onload = function (e) {
-    if (xhReqGuild.readyState === 4) {
-      if (xhReqGuild.status === 200) {
-        jsonObjectGuild = JSON.parse(xhReqGuild.responseText); // Contain the JSON in the variable
-      } else {
-        console.error(xhr.statusText);
-      }
-    }
-  };
-  xhReqGuild.onerror = function (e) {
-    console.error(xhReqGuild.statusText);
-  };
-  xhReqGuild.send(null);
-}
+  $.getJSON("https://eu.api.battle.net/wow/guild/argent-dawn/Cutthroat%20Comrades?fields=members&locale=en_GB&apikey=wr4u5hb5magzc44cc8usfum542fq7p3j", function(json){
+      jsonObjectGuild = json;
+  })
+};
 
-// Call the character API (used many times)
+  // Call the character API (used many times)
 function characterApiCall (){
   for (i = 0; i < jsonObjectGuild.members.length; i++) {
     if (jsonObjectGuild.members[i].character.level == "110") { // Only do things if a character is level 110
       characterName[i] = jsonObjectGuild.members[i].character.name;
       // Now to look up each character as we reach them in the loop
-      var xhReqChar = new XMLHttpRequest();
-      xhReqChar.open("GET", "https://eu.api.battle.net/wow/character/argent-dawn/" + characterName[i] + "?fields=items&locale=en_GB&apikey=wr4u5hb5magzc44cc8usfum542fq7p3j", false);
-      xhReqChar.send(null);
-      jsonObjectCharacter = JSON.parse(xhReqChar.responseText);
-      // We should now have jsonObjectCharacter detailing the current character's items and armour
-      toPrint += "<li><em>Name</em>: " + jsonObjectGuild.members[i].character.name + ", <em>Character Item Level</em>: " + jsonObjectCharacter.items.averageItemLevel + "</li><br>" ;
+      $.getJSON("https://eu.api.battle.net/wow/character/argent-dawn/" + characterName[i] + "?fields=items&locale=en_GB&apikey=wr4u5hb5magzc44cc8usfum542fq7p3j", function(json){
+          jsonObjectCharacters.push(json);
+          // We are now building up jsonObjectCharacters with all the characters' data
+      })
     }
   } // Finished with FOR loop code
 }
 
 function finishOff() {
   console.log("Finished, for better or worse! Now trying to print character names and their iLvls.")
+  // Code to organise the character array to go here
+  // as-yet unwritten code
+  // Now to build the thing to actually print. Note to self: might look good as HTML table
+  for (j = 0; j < jsonObjectCharacters.length; j++) {
+    toPrint += "Name: "
+    toPrint += jsonObjectCharacters[j].name
+    toPrint += " iLvl: "
+    toPrint += jsonObjectCharacters[j].items.averageItemLevel
+    toPrint += "<br>"
+  }
   infoDiv.style.display = "none";
   characterList.innerHTML = toPrint;
-  loadingDiv.style.display = "none";
+  $(".loading-Spinner").slideUp();
 }
 
 // Listen for the button to be pressed and call and return the data we need using the above function
 refreshButton.addEventListener("click", () => {
-  loadingDiv.style.display="block";
+  $(".loading-Spinner").slideDown();
   console.log("Thanks for clicking that button! Now to get to work.");
   guildApiCall();
   setTimeout(characterApiCall, 5000);
   setTimeout(finishOff, 10000);
-}) // Finished with button click code
+}); // Finished with button click code
